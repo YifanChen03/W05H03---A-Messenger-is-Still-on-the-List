6de38378212f78f0eee93df837565a2c867537a2
cyanfhine <yifan03.chen@tum.de>
2022-11-25 16:53:17 +0100

    pinguTalk implementiert

--- src/pgdp/messenger/PinguTalk.java
+++ src/pgdp/messenger/PinguTalk.java
@@ -1,14 +1,86 @@
 package pgdp.messenger;
 
 public class PinguTalk {
-	// TODO: Implementiere die fehlenden Attribute
-    private long topicID;
-    private long userID;
+    // TODO: Implementiere die fehlenden Attribute
+    private static long topicID;
+    private static long userID;
+
+    private UserArray users;
+    private Topic[] topics;
 
     // TODO: Implementiere den fehlenden Konstruktor
-    public PinguTalk(int initCapacity, int userID) {
+    public PinguTalk(int initCapacity, int topicLength) {
+        if (initCapacity > 0) {
+            users = new UserArray(initCapacity);
+        } else {
+            users = new UserArray(1);
+        }
 
+        if (topicLength > 0) {
+            topics = new Topic[topicLength];
+        } else {
+            topics = new Topic[1];
+        }
     }
 
     // TODO: Implementiere die fehlenden Methoden
+    public User addMember(String name, User supervisor) {
+        User r_user = new User(userID, name, supervisor);
+        users.addUser(r_user);
+        userID++;
+        return r_user;
+    }
+
+    public User deleteMember(long id) {
+        return users.deleteUser(id);
+    }
+
+    public Topic createNewTopic(String name) {
+        Topic n_topic = new Topic(topicID, name);
+
+        if (topics != null) {
+            for (int i = 0; i < topics.length; i++) {
+                if (topics[i] == null) {
+                    topics[i] = n_topic;
+                    topicID++;
+                    return n_topic;
+                }
+            }
+            //Array ist voll
+            return null;
+        }
+
+        return n_topic;
+    }
+
+    public Topic deleteTopic(long id) {
+        if (topics != null) {
+            for (int i = 0; i < topics.length; i++) {
+                if (topics[i].getId() == id) {
+                    Topic t_topic = topics[i];
+                    topics[i] = null;
+                    return t_topic;
+                }
+            }
+        }
+        return null;
+    }
+
+    //Getter und Setter
+
+    public static long getTopicID() {
+        return topicID;
+    }
+
+    public static void setTopicID(long topicID) {
+        PinguTalk.topicID = topicID;
+    }
+
+    public static long getUserID() {
+        return userID;
+    }
+
+    public static void setUserID(long userID) {
+        PinguTalk.userID = userID;
+    }
 }
